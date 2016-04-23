1ddcb0fc8a32dd1c05b3d5ca23415f868555cc94
dainius123 <lopas489756>
2016-04-24 00:45:22 +0300

    Comments done. TODO - write to file

--- src/main/java/Crawler.java
+++ src/main/java/Crawler.java
@@ -2,17 +2,17 @@ import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.WebCrawler;
 import edu.uci.ics.crawler4j.parser.HtmlParseData;
 import edu.uci.ics.crawler4j.url.WebURL;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.DataNode;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 import java.util.regex.Pattern;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-import org.jsoup.nodes.Element;
 
 /**
  * Created by dovile on 2016-04-19.
@@ -69,6 +69,14 @@ public class Crawler extends WebCrawler
             catch (IOException e){
                 e.printStackTrace();
             }
+
+            try {
+                parseComment(url + "?comments");
+
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
         }
     }
 
@@ -77,7 +85,7 @@ public class Crawler extends WebCrawler
 
         boolean s = false;
         int number =1;
-        while(s!=true){
+       // while(!s){
 
             String fileName = "./data/"+name+".txt";
             File yourFile = new File(fileName);
@@ -91,17 +99,86 @@ public class Crawler extends WebCrawler
                 writer.close();
                 s=true;
 
-            }else {
-                number++;
-                //System.out.println(yourFile.getAbsolutePath());
             }
-
-        }
+     //   }
 
 
 
 
     }
+
+    private void parseComment(String url) throws IOException {
+
+        Document doc = Jsoup.connect(url).get();
+        String authorName = "";
+        String date = "";
+        /*STRAIPSNIS*/
+        //Tema
+        //Autorius
+//        Element authorDIV = doc.select("div.author-info").first();
+//        if (authorDIV != null) {
+//            authorName = authorDIV.tagName("a.name").text();
+//        }
+
+        Elements scriptElements = doc.getElementsByTag("script");
+        String script="";
+        for (Element element :scriptElements ){
+            for (DataNode node : element.dataNodes()) {
+                if(node.getWholeData().contains("div.comments-placeholder")){
+                    script = node.getWholeData();
+                    script = script.substring(41);
+                    script = script.substring(0,script.length()-1);
+                    System.out.println(script);
+                }
+            }
+        }
+
+
+        JSONObject obj = new JSONObject(script);
+        //String pageName = obj.getJSONObject("comments").getString("pageName");
+
+        JSONArray comments = obj.getJSONArray("comments");
+        for (int i = 0; i < comments.length(); i++)
+        {
+            String comment = comments.getJSONObject(i).getString("comment");
+            JSONArray replies = comments.getJSONObject(i).getJSONArray("replies");
+            getReplies(replies);
+            System.out.println(i + " " +comment);
+            System.out.println("-------------------");
+        }
+
+
+        //Pavadinimas
+
+//        String articalName = h1.substring(0, 20) + doc.select("div").attr("data-id");
+//
+//        //Data
+//        Element dateMeta = doc.select("meta[itemprop=datePublished]").first();
+//
+//        if (dateMeta != null) {
+//            ents media = doc.select("[src]");
+//            Elements imports = doc.select("link[href]");*/
+//
+//
+//            if (h1 != null && articalText != null) {//straipsnis
+//                String articleInfo = url + "\n" + "Autorius: " + authorName + "\n" + "Pavadinimas: " +
+//                        h1 + "\n" + "Laikas: " + time + "\n" + "Data: " + date + "\n" + "Straipsnio tekstas: " + articalText + "\n";
+//                writeText(articleInfo, articalName);
+//            }
+//            date = dateMeta.attr("content");
+//        }
+    }
+    private void getReplies(JSONArray replies){
+
+        for (int i = 0; i < replies.length(); i++)
+        {
+            String comment = replies.getJSONObject(i).getString("comment");
+            System.out.println(i + " reply " +comment);
+            System.out.println("-------------------");
+        }
+    }
+
+
     private void parseHTML(String url) throws IOException{
 
         Document doc = Jsoup.connect(url).get();
@@ -144,51 +221,53 @@ public class Crawler extends WebCrawler
 
 
 
+
+
         /*KOMENTARAS*/
 
-        List<String[]> comments = new ArrayList<String[]>();
-        Element isComment = doc.select("div.comments-page").first();
-
-
-        if (isComment!=null){//komentaru puslapis
-
-            String articleID = doc.select("div").attr("data-id");
-
-            Elements commentsDIV= doc.select("div.item.clearfix.main-item");
-            for (Element div : commentsDIV) {
-
-                String[] commentInfo = new String[6];
-
-                //Numeris
-                commentInfo[0] = div.attr("data-item");
-                //Autorius
-                commentInfo[1]= doc.select("span.name").text();
-                //Data
-                commentInfo[2] = doc.select("span.timestamp").attr("title");
-                //Laikas
-                commentInfo[3] = doc.select("span.timestamp").text();
-                //IP
-                commentInfo[4] = doc.select("span.name").attr("title");
-                //Tekstas
-                commentInfo[5] = doc.select("div.comment").text();
-
-                comments.add(commentInfo);
-
-            }
-           for (String[] l:comments){
-
-               String commentInfo = "";
-               for (int i =0;i< l.length;i++){
-
-                   commentInfo = url+"\n"+"Autorius: "+l[1]+"\n"+"Numeris: "+
-                           l[0]+"\n"+"Laikas: "+l[3]+"\n"+"Data: "+l[2]+"\n"+"IP: "+l[4]+"\n"
-                   +"Komentaro tekstas: "+l[5]+"\n";
-
-
-               }
-               if (!commentInfo.isEmpty()){
-                   writeText(commentInfo,"komentaras"+comments.indexOf(l)+"tekstoID:"+articleID);
-               }
+//        List<String[]> comments = new ArrayList<String[]>();
+//        Element isComment = doc.select("div.comments-page").first();
+//
+//
+//        if (isComment!=null){//komentaru puslapis
+//
+//            String articleID = doc.select("div").attr("data-id");
+//
+//            Elements commentsDIV= doc.select("div.item.clearfix.main-item");
+//            for (Element div : commentsDIV) {
+//
+//                String[] commentInfo = new String[6];
+//
+//                //Numeris
+//                commentInfo[0] = div.attr("data-item");
+//                //Autorius
+//                commentInfo[1]= doc.select("span.name").text();
+//                //Data
+//                commentInfo[2] = doc.select("span.timestamp").attr("title");
+//                //Laikas
+//                commentInfo[3] = doc.select("span.timestamp").text();
+//                //IP
+//                commentInfo[4] = doc.select("span.name").attr("title");
+//                //Tekstas
+//                commentInfo[5] = doc.select("div.comment").text();
+//
+//                comments.add(commentInfo);
+//
+//            }
+//           for (String[] l:comments){
+//
+//               String commentInfo = "";
+//               for (int i =0;i< l.length;i++){
+//
+//                   commentInfo = url+"\n"+"Autorius: "+l[1]+"\n"+"Numeris: "+
+//                           l[0]+"\n"+"Laikas: "+l[3]+"\n"+"Data: "+l[2]+"\n"+"IP: "+l[4]+"\n"
+//                   +"Komentaro tekstas: "+l[5]+"\n";
+//
+//
+//               }
+//               if (!commentInfo.isEmpty()){
+//                   writeText(commentInfo,"komentaras"+comments.indexOf(l)+"tekstoID:"+articleID);
+//               }
 
 
            }
@@ -205,5 +284,4 @@ public class Crawler extends WebCrawler
 
 
 
-    }
-}
+
